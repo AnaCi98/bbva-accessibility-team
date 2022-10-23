08790917f4246ba21ea542c9ad0ada2f8f2d76be
KiraBelak <kalebchivas@hotmail.com>
2022-10-23 01:29:47 -0600

    cambiso de el ya jala

--- index.php
+++ index.php
@@ -4,6 +4,7 @@ include_once 'conexion.php';
 <!DOCTYPE html>
 <html lang="en">
 <head>
+  
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <link rel="stylesheet" href="style.css" />
